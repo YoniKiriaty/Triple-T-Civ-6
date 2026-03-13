2725aa7f6f0d83aa2f7be99135df567b80a2bb95
Yoni Kiriaty <YoniKiriaty@gmail.com>
2026-03-13 22:02:30 +0200

    Yoni - fixed leader instead of civilization in config

--- Config/Config.sql
+++ Config/Config.sql
@@ -5,12 +5,12 @@ VALUES  ('LEADER_TRIPLE_T_DSH_NAME', 'Unique', 'COLOR_PLAYER_WHITE_TEXT', 'COLOR
 -- 2. Define the Civ and Leader for the Setup Screen
 INSERT INTO Players (Domain, CivilizationType, CivilizationName, CivilizationIcon, CivilizationAbilityName, CivilizationAbilityDescription, CivilizationAbilityIcon, LeaderType, LeaderName, LeaderIcon, LeaderAbilityName, LeaderAbilityDescription, LeaderAbilityIcon)
 VALUES  ('Players:Expansion2_Players', 
-         'CIVILIZATION_TRIPLE_T_DSH_NAME', 
+         'CIVILIZATION_BRAINROT_DSH_NAME', 
          'LOC_CIVILIZATION_BRAINROT_DSH_NAME', 
-         'ICON_CIVILIZATION_TRIPLE_T_DSH_NAME', 
+         'ICON_CIVILIZATION_BRAINROT_DSH_NAME', 
          'LOC_TRAIT_CIVILIZATION_BRAINROT_DSH_ABILITY_NAME', 
          'LOC_TRAIT_CIVILIZATION_BRAINROT_DSH_ABILITY_DESCRIPTION', 
-         'ICON_CIVILIZATION_TRIPLE_T_DSH_NAME', 
+         'ICON_CIVILIZATION_BRAINROT_DSH_NAME', 
          'LEADER_TRIPLE_T_DSH_NAME', 
          'LOC_LEADER_TRIPLE_T_DSH_NAME', 
          'ICON_LEADER_TRIPLE_T_DSH_NAME', 
@@ -20,5 +20,5 @@ VALUES  ('Players:Expansion2_Players',
 
 -- 3. Link UI items (Placeholders: Using standard Swordsman and Monument so the UI shows something)
 INSERT INTO PlayerItems (Domain, CivilizationType, LeaderType, Type, Icon, Name, Description, SortIndex)
-VALUES  ('Players:Expansion2_Players', 'CIVILIZATION_TRIPLE_T_DSH_NAME', 'LEADER_TRIPLE_T_DSH_NAME', 'UNIT_SWORDSMAN', 'ICON_UNIT_SWORDSMAN', 'LOC_UNIT_SWORDSMAN_NAME', 'LOC_UNIT_SWORDSMAN_DESCRIPTION', 10),
-        ('Players:Expansion2_Players', 'CIVILIZATION_TRIPLE_T_DSH_NAME', 'LEADER_TRIPLE_T_DSH_NAME', 'BUILDING_MONUMENT', 'ICON_BUILDING_MONUMENT', 'LOC_BUILDING_MONUMENT_NAME', 'LOC_BUILDING_MONUMENT_DESCRIPTION', 20);
+VALUES  ('Players:Expansion2_Players', 'CIVILIZATION_BRAINROT_DSH_NAME', 'LEADER_TRIPLE_T_DSH_NAME', 'UNIT_SWORDSMAN', 'ICON_UNIT_SWORDSMAN', 'LOC_UNIT_SWORDSMAN_NAME', 'LOC_UNIT_SWORDSMAN_DESCRIPTION', 10),
+        ('Players:Expansion2_Players', 'CIVILIZATION_BRAINROT_DSH_NAME', 'LEADER_TRIPLE_T_DSH_NAME', 'BUILDING_MONUMENT', 'ICON_BUILDING_MONUMENT', 'LOC_BUILDING_MONUMENT_NAME', 'LOC_BUILDING_MONUMENT_DESCRIPTION', 20);
